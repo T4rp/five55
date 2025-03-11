515af48309f198b84935bfbafd67e6ba5daf1b2c
T4rp <why9ten@gmail.com>
2025-03-11 08:08:43 -0400

    feat: simple echoing

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
@@ -10,5 +12,23 @@ func main() {
 
 	if token == "" {
 		log.Fatal("No token provided")
+	}
+
+	session, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	session.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.Bot {
+			return
+		}
+
+		s.ChannelMessageSend(m.ChannelID, m.Content)
+	})
+
+	err = session.Open()
+	if err != nil {
+		log.Fatal(err)
 	}
 }
