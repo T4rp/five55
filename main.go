e8dbe7557910b97a16f8187f6199f754e9b95585
T4rp <why9ten@gmail.com>
2025-03-11 11:34:03 -0400

    feat: Add interation support

--- main.go
+++ main.go
@@ -3,15 +3,26 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
 
 	"github.com/bwmarrin/discordgo"
 )
+
+type BotContext struct {
+	application_id string
+	session        *discordgo.Session
+}
 
 func main() {
 	token := os.Getenv("TOKEN")
 
 	if token == "" {
 		log.Fatal("No token provided")
+	}
+
+	app_id := os.Getenv("APP_ID")
+	if token == "" {
+		log.Fatal("No application id provided")
 	}
 
 	test_guild := os.Getenv("TEST_GUILD")
@@ -42,8 +53,53 @@ func main() {
 		}
 	})
 
+	session.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.Bot {
+			return
+		}
+
+		_, err := s.ChannelMessageSendComplex(m.ChannelID, &discordgo.MessageSend{
+			Content: m.Content,
+			Embeds: []*discordgo.MessageEmbed{{
+				Title:       "Rahh",
+				Description: "Rahhhhh",
+				Color:       0xaaff96,
+			}},
+		})
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
+	session.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if i.ID == "info" {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "hello world",
+				},
+			})
+		} else {
+			log.Println("Cannot handle interaction with id: " + i.ID)
+		}
+	})
+
+	_, err = session.ApplicationCommandCreate(app_id, test_guild, &discordgo.ApplicationCommand{
+		Name:        "info",
+		Description: "ARarararh",
+	})
+	if err != nil {
+		log.Println("Failed to create application command")
+	}
+
 	err = session.Open()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	stopSignal := make(chan os.Signal, 1)
+	signal.Notify(stopSignal, os.Interrupt)
+	<-stopSignal
+
+	log.Println("Stopping...")
 }
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-TOKEN="yuh" TEST_GUILD="0" go run .
+TOKEN="yuh" TEST_GUILD="0" APP_ID="0" go run .
